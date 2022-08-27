d09cf8294f2f3267253499352edd970c8cacbfe1
gaochao <1915530614@qq.com>
2022-08-27 16:44:04 +0800

    third commit

--- src/main/java/com/atguigu/gittest/gitTest.java
+++ src/main/java/com/atguigu/gittest/gitTest.java
@@ -7,5 +7,6 @@ package com.atguigu.gittest;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello git-test");
+        System.out.println("Hello git-test");
     }
 }
